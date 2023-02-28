5f40d77c11daf8488e78ee6b891d319162d5076f
ShuYu <10892892+shuyu520@user.noreply.gitee.com>
2023-02-28 23:21:54 +0800

    feat: 登录页面布局

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue'
 
 defineProps({
   msg: String,
+  name: "Home"
 })
 
 const count = ref(0)
@@ -21,9 +22,8 @@ const count = ref(0)
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
+    starter
   </p>
   <p>
     Install
