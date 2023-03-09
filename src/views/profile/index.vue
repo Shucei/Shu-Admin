16ddd680d4a1d6ddb4f4b837f008d9834f23b16e
ShuYu <10892892+shuyu520@user.noreply.gitee.com>
2023-03-09 20:19:20 +0800

    feat: 个人中心左侧信息

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,21 +1,39 @@
 <template>
-  <div class="">
-    主页
+  <div class="my-container">
     <el-row>
-      <el-button>Default</el-button>
-      <el-button type="primary">Primary</el-button>
-      <el-button type="success">Success</el-button>
-      <el-button type="info">Info</el-button>
-      <el-button type="warning">Warning</el-button>
-      <el-button type="danger">Danger</el-button>
+      <el-col :span="8">
+        <project-card class="user-card"></project-card>
+      </el-col>
+      <el-col :span="15">
+        <el-card>
+          <el-tabs v-model="activeName">
+            <el-tab-pane :label="$t('msg.profile.information')" name="information">
+              <Information />
+            </el-tab-pane>
+            <el-tab-pane :label="$t('msg.profile.account')" name="account">
+              <Account />
+            </el-tab-pane>
+          </el-tabs>
+        </el-card>
+
+      </el-col>
     </el-row>
+
   </div>
 </template>
 
 <script setup>
-
+import ProjectCard from './components/ProjectCard.vue'
+import Information from './components/information.vue'
+import Account from './components/Account.vue'
+import { ref } from 'vue'
+const activeName = ref('feature')
 </script>
 
 <style lang="scss" scoped>
-
+.my-container {
+  .user-card {
+    margin-right: 20px;
+  }
+}
 </style>
